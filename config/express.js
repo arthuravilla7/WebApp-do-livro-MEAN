7c50c7a0858c9088bda6edba2cee61bf62f2d0e6
Arhur Avilla Santos <arthursantos@add.addtech.com.br>
2017-01-27 18:15:57 -0200

    livro MEAN pg 25 - 43 do pdf

---
+++ config/express.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var home = require('../app/routes/home');
+
+module.exports = function(){
+	var app = express();
+	
+	// configuração de ambiente
+	app.set('port', 3000);
+	
+	//middleware
+	app.use(express.static('./public'));
+	home(app);
+	
+	app.set('view engine', 'ejs');
+	app.set('views', './app/views');
+	
+	return app;
+}
