5ed69a5878ea11fe1022e2f2e37d4e2b1b3c5e94
Arhur Avilla Santos <arthursantos@add.addtech.com.br>
2017-02-08 15:36:36 -0200

    Aplicacao se comunicando com o MongoDB - CRUD - Pag182

---
+++ consulta.js
@@ -0,0 +1,24 @@
+var MongoClient = require('mongodb').MongoClient;
+var ObjectID = require('mongodb').ObjectID;
+
+var _idProcurado = new ObjectID('5898ad465ee31966c9edc1d5');
+
+MongoClient.connect('mongodb://127.0.0.1:27017/contatooh',
+	function(erro,db){
+		if(erro)
+			{
+				console.log("erro");
+				throw err;
+			}	
+		db.collection('contatos').findOne({_id : _idProcurado},
+			function(erro, contato){
+				if(erro)
+					{
+						console.log("erro");
+						throw err;
+					}	
+				console.log(contato);
+			}
+		);	
+	}
+);
